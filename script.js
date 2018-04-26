b98dc4f75eb9152bfd25ca83f0a428825305cb3b
Marcin <lubera.marcin@gmail.com>
2018-04-26 19:06:58 +0200

    dodany footer

--- script.js
+++ script.js
@@ -18,3 +18,18 @@
     
 
 })();
+
+$(function(){
+ 
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 100) {
+			$('.scrollTopButton').addClass('show');
+		} else {
+			$('.scrollTopButton').removeClass('show');
+		}
+	});
+ 
+	$('.scrollTopButton').click(function() {
+		$('body').animate({scrollTop: 0}, 400, 'linear');
+	});
+});
