7949dc50f75324956300f3cd7a8e8c3f0b62d2d5
Marcin <lubera.marcin@gmail.com>
2018-06-11 17:10:22 +0200

    added jquery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-
-$(".js-hmbg-menu").click(function() {
-    $('.js-menu-mobile').toggle('is-active');
-});
+(function() {
+    $(document).ready((function(){
+        $(".js-hmbg-menu").click(function() {
+            $('.js-menu-mobile').toggle('is-active');
+        });
+    }));
+}());
